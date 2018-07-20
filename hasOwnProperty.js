f688435401a1da8a52efa942828ffcdae91c0378
Fabio Vergani <fabio.vergani.1975@gmail.com>
2018-07-20 12:31:37 +0200

    Update hasOwnProperty.js

--- hasOwnProperty.js
+++ hasOwnProperty.js
@@ -1,4 +1,21 @@
-Object.prototype.hasOwnProperty||Object.prototype.hasOwnProperty=function(x){
- var e=this,p=String(x),o=e.__proto__||e.constructor.prototype;
- return p in e && (!(p in o)||e[p]!== o[p])
-};
+//#Polyfill
+Object.prototype.hasOwnProperty||(Object.prototype.hasOwnProperty=function(x){
+ var o,e=this,p=String(x);
+ return p in e && (o=e.__proto__||e.constructor.prototype,!(p in o)||e[p]!== o[p])
+});
+
+/*
+var obj1={a:1,b:false}, obj2=Object.create(obj1);
+obj2.a=2;
+obj2.c='yeah';
+//Polyfilled & Native:
+console.dir([
+obj1.hasOwnProperty('a'),//true
+obj1.hasOwnProperty('b'),//true
+obj1.hasOwnProperty('c'),//false
+obj2.hasOwnProperty('a'),//true
+obj2.hasOwnProperty('b'),//false
+obj2.hasOwnProperty('c')//true
+]);
+Object.prototype.hasOwnProperty.call(Object.create(null),'a')//false
+*/
